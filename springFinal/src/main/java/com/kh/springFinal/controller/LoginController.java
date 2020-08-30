781bdff4ef2706760c7ee8ad480e7feb53acd440
jinbin <gap5911@naver.com>
2020-08-30 19:50:09 +0900

    로그인수정

--- springFinal/src/main/java/com/kh/springFinal/controller/LoginController.java
+++ springFinal/src/main/java/com/kh/springFinal/controller/LoginController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/member")
+@RequestMapping("/")
 public class LoginController {
 
-	@GetMapping("/login")
+	@GetMapping("/")
 	public String login() {
-		return "member/login";
+		return "login";
 	}
 
 }
